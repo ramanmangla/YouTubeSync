182c300c9c9b32490779397ff137c0a01ee784c2
Raman Mangla <ramanmangla7@gmail.com>
2019-08-13 13:35:42 -0400

    Added YouTube embedded player with some styling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 var app = express();
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => res.render("index"));
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
