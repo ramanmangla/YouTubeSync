a2e4fe9e22a57f051400a0435d1898b2a5c93fd8
Raman Mangla <ramanmangla7@gmail.com>
2019-08-13 16:27:13 -0400

    Added socket.io for two way communication

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+var app = express();
+const http = require('http').Server(app);
+var io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000;
 
-var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -10,3 +12,7 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+io.on('connection', socket => {
+  console.log('a user connected');
+});
